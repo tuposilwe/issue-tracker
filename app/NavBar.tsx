4b4b43b3780a9f1d3acd3cd997085414452cd588
tuposilwe <rudigermkondya@gmail.com>
2023-10-15 11:12:02 -0700

    Remove duplicate skeletons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -12,7 +12,7 @@ const currentPath = usePathname();
 
 const links = [
     {label: 'Dashboard', href:'/'},
-    {label: 'Issues', href:'/issues'}
+    {label: 'Issues', href:'/issues/list'}
 ]
 
   return (
--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -15,7 +15,7 @@ const deleteIssue =  async () => {
     try {
         setDeleting(true);
         await axios.delete('/api/issues/' + issueId);
-        router.push('/issues');
+        router.push('/issues/list');
         router.refresh();
     } catch (error) {
         setDeleting(false);
