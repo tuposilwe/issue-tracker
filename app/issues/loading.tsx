a66b91241a624d66301519b79ba75dd3e93faa82
tuposilwe <rudigermkondya@gmail.com>
2023-10-14 23:37:59 -0700

    Refactor:Organize import statements

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,9 +1,6 @@
-import { Table } from '@radix-ui/themes'
-import React from 'react'
-import IssueStatusBadge from '../components/IssueStatusBadge'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import IssueActions from './IssueActions'
+import { Skeleton } from '@/app/components';
+import { Table } from '@radix-ui/themes';
+import IssueActions from './IssueActions';
 
 function LoadingIssuesPage() {
   const issues = [1,2,3,4,5];
