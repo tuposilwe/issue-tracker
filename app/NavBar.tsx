4b7d25dfd20837aa6e280bb1f521bd7b7bd68bfb
tuposilwe <rudigermkondya@gmail.com>
2023-10-17 06:31:02 -0700

    Fix avatar not loading

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -57,10 +57,11 @@ const NavBar = () => {
                 <DropdownMenu.Trigger>
                   <Avatar
                     src={session.user!.image!}
-                    fallback="T"
+                    fallback="?"
                     size="2"
                     radius="full"
                     className="cursor-pointer"
+                    referrerPolicy='no-referrer'
                   />
                 </DropdownMenu.Trigger>
                 <DropdownMenu.Content>
