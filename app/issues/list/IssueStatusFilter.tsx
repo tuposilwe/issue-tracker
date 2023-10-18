3ec89d6ac52c33dface4018f7b5f5af34782c9cd
tuposilwe <rudigermkondya@gmail.com>
2023-10-18 03:17:50 -0700

    Fix filtering bugs

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
-const statuses: { label: string, value?: Status }[] = [
+const statuses: { label: string; value?: Status }[] = [
   { label: "All" },
   { label: "Open", value: "OPEN" },
   { label: "In Progress", value: "IN_PROGRESS" },
@@ -12,17 +12,29 @@ const statuses: { label: string, value?: Status }[] = [
 ];
 
 const IssueStatusFilter = () => {
-const router = useRouter();
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
-    <Select.Root onValueChange={(status) =>{
-        const query = status === 'ALL' ? '' : `?status=${status}`;
-        router.push('/issues/list'+query)
-    } }>
+    <Select.Root
+      defaultValue={searchParams.get("status") || ''}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+
+        // const query = status === 'ALL' ? '' : `?status=${status}`;
+        const query = params.size ? "?" + params.toString() : "";
+        router.push("/issues/list" + query);
+      }}
+    >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
-        {statuses.map( status => (
-          <Select.Item key={status.value} value={status.value ?? 'ALL'}>{status.label}</Select.Item>
+        {statuses.map((status) => (
+          <Select.Item key={status.value} value={status.value ?? "ALL"}>
+            {status.label}
+          </Select.Item>
         ))}
       </Select.Content>
     </Select.Root>
